f6655a27af3810cd67706aeea195bdcd61d057d4
Andrew Guard <andrewguard@clswl113.cels.anl.gov>
2017-09-21 15:47:56 -0500

    Tooltip and styling - subsystems

--- public/js/p3/widget/SubSystemsOverviewMemoryGrid.js
+++ public/js/p3/widget/SubSystemsOverviewMemoryGrid.js
@@ -1,10 +1,10 @@
 define([
   "dojo/_base/declare", "dijit/layout/BorderContainer", "dojo/on", "dojo/_base/Deferred",
-  "dojo/dom-class", "dijit/layout/ContentPane", "dojo/dom-construct",
+  "dojo/dom-class", "dijit/layout/ContentPane", "dojo/dom-construct", "dijit/Tooltip",
   "dojo/_base/xhr", "dojo/_base/lang", "./PageGrid", "./formatter", "../store/SubsystemsOverviewMemoryStore", "dojo/request",
   "dojo/aspect", "./GridSelector", "dojo/when", "d3/d3", "dojo/Stateful", "dojo/topic", "../util/PathJoin", "dojo/promise/all"
 ], function(declare, BorderContainer, on, Deferred,
-      domClass, ContentPane, domConstruct,
+      domClass, ContentPane, domConstruct, Tooltip,
       xhr, lang, Grid, formatter, SubsystemsOverviewMemoryStore, request,
       aspect, selector, when, d3, Stateful, Topic, PathJoin, All){
   return declare([Stateful], {
@@ -112,15 +112,17 @@ define([
         .attr('width', width)
         .attr('height', height)
         .append('g')
-        .attr('transform', 'translate(' + (height / 2 + 100) +
+        .attr('transform', 'translate(' + (height / 2 + 200) +
           ',' + (height / 2 + 50) + ')');
 
       d3.select('#subsystemspiechart svg')
         .append("text")
-        .attr("x", height / 2 + 100)             
+        .attr("x", height / 2 + 200)             
         .attr("y", 50)
         .attr("text-anchor", "middle")
-        .text(titleText);
+        .style("font-weight", "bold")
+        .style("font-size", "13px")
+        .text("Subsystem Category Distribution - " + titleText);
 
       var arc = d3.svg.arc()
         .innerRadius(0)
@@ -136,7 +138,7 @@ define([
         .append('path')
         .attr('d', arc)
         .on("mouseover", function(d) {
-          return tooltip.style("visibility", "visible").text(d.data.val);
+          return tooltip.style("visibility", "visible").text(d.data.val + " (" + d.data.count + ")");
         })
         .on("click", function(d) {
           that.navigateToSubsystemsSubTab(d);
@@ -151,19 +153,30 @@ define([
 
       var legendHolder = svg.append('g')
         .attr('transform', "translate(" + (margin.left + radius) + ",0)")
+        .attr("id", "legendHolder");
 
       var subsystemslegend = legendHolder.selectAll('.subsystemslegend')
-          .data(color.domain())
-          .enter()
-          .append('g')
-          .attr('class', 'subsystemslegend')
-          .attr('transform', function(d, i) {
-            var height = legendRectSize + legendSpacing;
-            var offset =  height * color.domain().length / 2;
-            var horz = -2 * legendRectSize;
-            var vert = i * height - offset;
-            return 'translate(' + horz + ',' + vert + ')';
-        });
+        .data(color.domain())
+        .enter()
+        .append('g')
+        .attr('class', 'subsystemslegend')
+        .attr('transform', function(d, i) {
+          var height = legendRectSize + legendSpacing;
+          var offset =  height * color.domain().length / 2;
+          var horz = -2 * legendRectSize;
+          var vert = i * height - offset;
+          return 'translate(' + horz + ',' + vert + ')';
+      });
+
+      var legendCount = legendHolder.selectAll('.subsystemslegend').size();
+      var legendTitleOffset = legendCount * legendRectSize / 2 + 50;
+
+      legendHolder.append('text')
+        .attr('x', 0)
+        .attr('y', -legendTitleOffset)
+        .style("font-weight", "bold")
+        .style("font-size", "13px")
+        .text("Subsystem Feature Counts");
 
       subsystemslegend.append('rect')
           .attr("x", 0)
@@ -194,7 +207,7 @@ define([
 
       if (this.genomeView) {
         var summaryBarWidth = width / 4;
-        var summaryBarHeight = height - 200;
+        var summaryBarHeight = height;
 
         this.getSubsystemCoverageData(summaryBarWidth, summaryBarHeight);
       }
@@ -314,45 +327,68 @@ define([
       var totalHeight = divHeightCovered + divHeightNotCovered;
 
       var svg = d3.select("#subsystemspiechart svg"),
-          margin = {top: 20, right: 20, bottom: 30, left: 40},
+          margin = {top: 0, right: 20, bottom: 30, left: 100},
           width = +svg.attr("width") - margin.left - margin.right,
           height = +svg.attr("height") - margin.top - margin.bottom,
           g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
       var coveredRect = svg.append("rect")
-                          .attr("x", 70)
-                          .attr("y", height / 5)
+                          .attr("x", 120)
+                          .attr("y", height / 7)
                           .attr("width", 50)
                           .style("fill", "#399F56")
+                          .attr("id", "subsystemsCovered")
                           .attr("height", divHeightCovered);
 
       var notCoveredRect = svg.append("rect")
-                            .attr("x", 70)
-                            .attr("y", height / 5 + divHeightCovered)
+                            .attr("x", 120)
+                            .attr("y", height / 7 + divHeightCovered)
                             .attr("width", 50)
                             .style("fill", "#3F6993")
+                            .attr("id", "subsystemsNotCovered")
                             .attr("height", divHeightNotCovered);
 
       svg.append("text")
-        .attr("x", 100)             
-        .attr("y", height / 6)
+        .attr("x", 150)             
+        .attr("y", height / 8)
         .attr("text-anchor", "middle")
+        .style("font-weight", "bold")
+        .style("font-size", "13px")
         .text("Subsystem Coverage");
 
       //percentages
       svg.append("text")
-        .attr("x", 95)             
-        .attr("y", divHeightCovered / 2 + height / 5)
+        .attr("x", 145)             
+        .attr("y", divHeightCovered / 2 + height / 7)
         .attr("text-anchor", "middle")
         .style("fill", "#ffffff")
         .text(percentCovered + "%");
 
       svg.append("text")
-        .attr("x", 95)             
-        .attr("y", divHeightNotCovered / 2 + divHeightCovered + height / 5)
+        .attr("x", 145)             
+        .attr("y", divHeightNotCovered / 2 + divHeightCovered + height / 14)
         .attr("text-anchor", "middle")
         .style("fill", "#ffffff")
         .text(percentNotCovered + "%");
+
+      //reset print svg to include new graph after long api call
+      this.setSubsystemPieGraph();
+
+      new Tooltip({
+        connectId: ["subsystemsCovered"],
+        label: "<b>In Subsystem</b></br>"
+              + "Total (" + subsystemCoverageData.totalSubsystems + ")</br>"
+              + "Non-Hypothetical (" + subsystemCoverageData.totalSubsystemsNotHypothetical + ")</br>"
+              + "Hypothetical (" + subsystemCoverageData.totalSubsystemsHypothetical + ")"
+      });
+
+      new Tooltip({
+        connectId: ["subsystemsNotCovered"],
+        label: "<b>Not In Subsystem</b></br>"
+              + "Total (" + subsystemCoverageData.totalNotCovered + ")</br>"
+              + "Non-Hypothetical (" + subsystemCoverageData.totalNotCoveredNotHypothetical + ")</br>"
+              + "Hypothetical (" + subsystemCoverageData.totalNotCoveredHypothetical + ")"
+      });
     },
 
     navigateToSubsystemsSubTab: function(d) {
