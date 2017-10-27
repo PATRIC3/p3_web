313c90ad744cd093f3478b101c2d28aff98571a7
Joshua Sherman <joshua.v.sherman@gmail.com>
2017-10-27 10:45:01 -0400

    learning redis

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 const gulp = require('gulp');
 const replace = require('gulp-replace');
 const config = require('./config.js');
+const RedisServer = require('redis-server');
+const server = new RedisServer(6379);
 
 gulp.task('backend', function(){
   gulp.src(['./src/register.js'])
-    .pipe(replace('{$BackEndUrl}', config.get('backendUrl')))
-    .pipe(gulp.dest('./public/js/'));
-    gulp.src(['./src/user.js'])
-      .pipe(replace('{$BackEndUrl}', config.get('backendUrl')))
-      .pipe(gulp.dest('./public/js/'));
+  .pipe(replace('{$BackEndUrl}', config.get('backendUrl')))
+  .pipe(gulp.dest('./public/js/'));
+  gulp.src(['./src/user.js'])
+  .pipe(replace('{$BackEndUrl}', config.get('backendUrl')))
+  .pipe(gulp.dest('./public/js/'));
 });
+
+gulp.task('startredis'), function(){
+  server.open((err) => {
+    if (err === null) {
+      // You may now connect a client to the Redis
+      // server bound to `server.port` (e.g. 6379).
+    }
+  });
+}
