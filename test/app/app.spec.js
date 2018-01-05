1c243a37ca34df51a7bb80ed34546242e7b0d922
Joshua Sherman <joshua.v.sherman@gmail.com>
2018-01-05 12:10:50 -0500

    unit tests wip, linter fixes, and http patch req fix by change to post

--- test/app/app.spec.js
+++ test/app/app.spec.js
@@ -1,58 +1,44 @@
 const Test = require('../../src/app/app.js');
 const appTest = new Test();
-const winUtils = {get:function(){}};
-const lang = {hitch:function(){}};
-const on = function(){};
-const domStyle = {set:function(){}};
-const Deferred = class {constructor(something){}};
-const fx = {fadeIn:function(){return {play:function(){}}}};
-let Dialog = 'closed'
-
-test('defines the app', () => {
-  const appTest = new Test();
-  // let thisArray = [];
-  // let thisFunction = function(){};
-  //console.log(appTest.testobj);
-  //Test.define(thisArray, thisFunction);
-  //expect(document.body.innerHTML).toMatch(/Verify Your Email Address/);
-});
-
-// test('it calls the app define function', () => {
-//   const appTest = new Test();
-//   // let thisArray = [];
-//   // let thisFunction = function(){};
-//   appTest.testobj.testfunc();
-//   //console.log(appTest.testobj);
-//   //Test.define(thisArray, thisFunction);
-//   //expect(document.body.innerHTML).toMatch(/Verify Your Email Address/);
-// });
+const winUtils = {get:function() {}};
+const lang = {hitch:function() {}};
+const on = function() {};
+const domStyle = {set:function() {}};
+const Deferred = class {constructor(something) {}};
+const fx = {fadeIn:function() {return {play:function() {}};}};
+let Dialog = ['closed'];
 
 test('it creates the NMDialog', () => {
-appTest.testobj.testfunc(
-  'declare', 'parser',
-	'Topic', on, 'dom', 'domClass', 'domAttr',domStyle,
-	'Registry', 'xhr', 'ContentPane',fx,
-	Deferred,'query','nodeListDom',
-	'Ready', 'Parser', 'rql', lang,
-	'Router', Dialog, 'domConstruct', winUtils
-);
-  //console.log(appTest.testobj);
-  //Test.define(thisArray, thisFunction);
-  //expect(document.body.innerHTML).toMatch(/Verify Your Email Address/);
+  appTest.testobj.testfunc(
+    'declare', 'parser',
+    'Topic', on, 'dom', 'domClass', 'domAttr', domStyle,
+    'Registry', 'xhr', 'ContentPane', fx,
+    Deferred, 'query', 'nodeListDom',
+    'Ready', 'Parser', 'rql', lang,
+    'Router', Dialog, 'domConstruct', winUtils
+  );
 });
 
 test('it opens the NMDialog', () => {
-Dialog = 'open';
-appTest.testobj.testfunc(
-  'declare', 'parser',
-	'Topic', on, 'dom', 'domClass', 'domAttr',domStyle,
-	'Registry', 'xhr', 'ContentPane',fx,
-	Deferred,'query','nodeListDom',
-	'Ready', 'Parser', 'rql', lang,
-	'Router', Dialog, 'domConstruct', winUtils
-);
-//expect(result.promise).toBe('revolved');
-  //console.log(appTest.testobj);
-  //Test.define(thisArray, thisFunction);
-  //expect(document.body.innerHTML).toMatch(/Verify Your Email Address/);
+  Dialog = ['open'];
+  appTest.testobj.testfunc(
+    'declare', 'parser',
+    'Topic', on, 'dom', 'domClass', 'domAttr', domStyle,
+    'Registry', 'xhr', 'ContentPane', fx,
+    Deferred, 'query', 'nodeListDom',
+    'Ready', 'Parser', 'rql', lang,
+    'Router', Dialog, 'domConstruct', winUtils
+  );
+});
+
+test('The NMDialog knows when it has started, _alreadyInitialized, and fade out deferred', () => {
+  Dialog = ['closed', '_started', '_alreadyInitialized', '_fadeOutDeferred'];
+  appTest.testobj.testfunc(
+    'declare', 'parser',
+    'Topic', on, 'dom', 'domClass', 'domAttr', domStyle,
+    'Registry', 'xhr', 'ContentPane', fx,
+    Deferred, 'query', 'nodeListDom',
+    'Ready', 'Parser', 'rql', lang,
+    'Router', Dialog, 'domConstruct', winUtils
+  );
 });
