ec964c0c1d2d8f531dd2901069d67dca29fc1130
Cameron Beers <cb3xa@virginia.edu>
2019-08-22 16:31:13 -0400

    [UI] organize assets, rename files, add SASS files, include Bootstrap package, tweak menu JS

--- public/assets/js/patric_ui.js
+++ public/assets/js/patric_ui.js
@@ -1,24 +1,59 @@
 $(document).ready(function () {
 
-    $('#navbarDropdown_search').on('click', function (event) {
-        $(this).parent().toggleClass('show');
-        $(this).parent().find(".dropdown-menu").toggleClass("show");
-    });
 
-    // when you hover a toggle show its dropdown menu
-    $("#navbarDropdown_search").hover(function () {
-        if (!$(this).parent().find(".show")) {
-            $(this).parent().toggleClass("show");
-            $(this).parent().find(".dropdown-menu").toggleClass("show");
+    $('.dropdown-menu').hover(
+        function() {
+            $(this).addClass('show');
         }
+    );
 
-    });
+    // $('.dropdown-menu')
+    //     .mouseenter(function() {
+    //         $(this).addClass('show');
+    //         $(this).prev().addClass('show');
+    //     })
+    //     .mouseout(function() {
+    //         $(this).removeClass('show');
+    //     });
+
+
+    // $('.nav-item.dropdown').hover(
+    //     function() {
+    //         $(this).addClass('show');
+    //     }
+    // );
+
+    // Use this to slide the menu Down and Up
+    // $(".dropdown").hover(
+    //     function() {
+    //         $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+    //         $(this).toggleClass('show');
+    //     },
+    //     function() {
+    //         $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+    //         $(this).toggleClass('show');
+    //     }
+    // );
+
+    // $('#navbarDropdown_search').on('click', function (event) {
+    //     $(this).parent().toggleClass('show');
+    //     $(this).parent().find(".dropdown-menu").toggleClass("show");
+    // });
+
+    // // when you hover a toggle show its dropdown menu
+    // $("#navbarDropdown_search").hover(function () {
+    //     if (!$(this).parent().find(".show")) {
+    //         $(this).parent().toggleClass("show");
+    //         $(this).parent().find(".dropdown-menu").toggleClass("show");
+    //     }
+
+    // });
 
     // when you submit the search, hide the dropdown
-    $(".gs-advanced-search").click(function () {
-            $(".dropdown").removeClass("show");
-            $(".dropdown-menu").removeClass("show");
-    });
+    // $(".gs-advanced-search").click(function () {
+    //         $(".dropdown").removeClass("show");
+    //         $(".dropdown-menu").removeClass("show");
+    // });
 
 
     $('body').on('click', function (e) {
